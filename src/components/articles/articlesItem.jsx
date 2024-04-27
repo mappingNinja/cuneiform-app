e9db23d53bb397bafcc5297231b8e9b08bf9de2b
mappingNinja <mappingninja@gmail.com>
2024-04-27 13:14:14 +0530

    update

--- src/components/articles/articlesItem.jsx
+++ src/components/articles/articlesItem.jsx
@@ -60,48 +60,56 @@ const ArticlesItem = ({ searchQuery, isSortByDate, sortDirection }) => {
     <>
       {" "}
       <div className="article-item-container">
-        <div className="row">
-          {filteredArticles &&
-            filteredArticles?.map((article, index) => (
-              <div key={index} className="col-md-4 mb-4">
-                <div className="card" style={{ width: "100%" }}>
-                  <div className="card-body">
-                    <span
-                      className=" position-absolute top-0  translate-middle badge rounded-pill bg-secondary"
-                      style={{ left: "50%", zIndex: "1" }}
-                    >
-                      {article?.category}
-                    </span>
-                    <h5 className="card-title mt-3">{article?.title}</h5>
-                    <h6 className="card-slug mb-2 text-muted">
-                      {article?.slug}
-                    </h6>
-                    <p className="card-text">{article?.description}</p>
-                    <p className="card-text">
-                      <span className="fw-semibold">Created At: </span>
-                      {new Date(article?.createdAt).toLocaleDateString()}
-                    </p>
+        {filteredArticles?.length === 0 ? (
+          <div className="d-flex justify-content-center align-items-center vh-100">
+            <Link to={"/articleForm"}>
+              <button className="btn btn-secondary">Create Article</button>
+            </Link>
+          </div>
+        ) : (
+          <div className="row">
+            {filteredArticles &&
+              filteredArticles?.map((article, index) => (
+                <div key={index} className="col-md-4 mb-4">
+                  <div className="card" style={{ width: "100%" }}>
+                    <div className="card-body">
+                      <span
+                        className=" position-absolute top-0  translate-middle badge rounded-pill bg-secondary"
+                        style={{ left: "50%", zIndex: "1" }}
+                      >
+                        {article?.category}
+                      </span>
+                      <h5 className="card-title mt-3">{article?.title}</h5>
+                      <h6 className="card-slug mb-2 text-muted">
+                        {article?.slug}
+                      </h6>
+                      <p className="card-text">{article?.description}</p>
+                      <p className="card-text">
+                        <span className="fw-semibold">Created At: </span>
+                        {new Date(article?.createdAt).toLocaleDateString()}
+                      </p>
 
-                    <div className="d-flex justify-content-between align-items-center">
-                      <Link to={`/article/${article?.slug}`}>
-                        <button className="btn btn-primary">View</button>
-                      </Link>
-                      <div>
-                        <FaEdit
-                          className="text-primary"
-                          onClick={() => handleEdit(article)}
-                        />
-                        <FaTrash
-                          className="text-danger mx-2"
-                          onClick={() => handleDelete(article?.title)}
-                        />
+                      <div className="d-flex justify-content-between align-items-center">
+                        <Link to={`/article/${article?.slug}`}>
+                          <button className="btn btn-primary">View</button>
+                        </Link>
+                        <div>
+                          <FaEdit
+                            className="text-primary"
+                            onClick={() => handleEdit(article)}
+                          />
+                          <FaTrash
+                            className="text-danger mx-2"
+                            onClick={() => handleDelete(article?.title)}
+                          />
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            ))}
-        </div>
+              ))}
+          </div>
+        )}
       </div>
       <Modal isOpen={isOpen}>
         <ModalHeader toggle={() => setIsOpen(false)}>Edit Article</ModalHeader>
